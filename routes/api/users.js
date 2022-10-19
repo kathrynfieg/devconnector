ec477c5c842e879d9f516f2af82b9a4ae014092f
Kathryn Fieg <kathrynfieg@gmail.com>
2022-10-19 15:37:52 +1030

    implementing jwt

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
@@ -59,9 +61,23 @@ router.post(
       await user.save();
 
       // return jsonwebtoken (login user right away)
-      res.send('User registered');
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
 
-      res.send('User route');
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+
+      //   res.send('User route');
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
