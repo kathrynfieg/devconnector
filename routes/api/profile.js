797e3a2ffede3a7953b89fe5bcc5978da6742432
Kathryn Fieg <kathrynfieg@gmail.com>
2022-10-19 21:40:05 +1030

    delete profile and user endpoint

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -145,4 +145,24 @@ router.get('/user/:user_id', async (req, res) => {
   }
 });
 
+// @route   DELETE api/profile/user/:user_id
+// @desc    Delete profile, user, & posts
+// @access  Private
+router.delete('/', auth, async (req, res) => {
+  try {
+    //TODO: remove users posts
+
+    // remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+
+    // remove user
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: 'User deleted' });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
